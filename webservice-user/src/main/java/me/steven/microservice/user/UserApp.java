b00629a28213b3734f7945fb52d86e7e753bd92f
StevenSongHC <stevensonghc@gmail.com>
2017-02-04 17:43:33 +0800

    Adding ribbon support.

--- webservice-user/src/main/java/me/steven/microservice/user/UserApp.java
+++ webservice-user/src/main/java/me/steven/microservice/user/UserApp.java
@@ -2,6 +2,8 @@ package me.steven.microservice.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -10,10 +12,12 @@ import org.springframework.web.client.RestTemplate;
  * Created by Steven on 2017/1/10.
  */
 @EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserApp {
 
     @Bean
+    @LoadBalanced
     RestTemplate rest() {
         return new RestTemplate();
     }
